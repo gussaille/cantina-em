ab1c28138c1fca356d447e4f13bd1e69c5890a17
Nicolas Durocher <31470453+gussaille@users.noreply.github.com>
2019-09-09 19:23:12 +0200

    Modification du composant RecipeCard

--- src/components/List.vue
+++ src/components/List.vue
@@ -41,6 +41,6 @@ export default {
   display:flex;
   flex-wrap: wrap;
   align-items:center;
-  justify-content: space-around;
+  justify-content: space-between;
 }
 </style>
--- src/components/RecipeCard.vue
+++ src/components/RecipeCard.vue
@@ -1,7 +1,11 @@
 <template>
   <article class="recipe-card">
-    <h1>{{recipe.titre}}</h1>
-    <div class="recipe-"></div>
+    <h1 class="recipe-card__title">{{recipe.titre}}</h1>
+    <div class="informations">
+      <img :src="recipe.photo" class="informations__illustration" :alt="recipe.titre">
+      <p class="informations__level">{{ recipe.niveau }}</p>
+      <p class="informations__time"></p>
+    </div>
   </article>
 </template>
 
@@ -24,15 +28,38 @@ export default {
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
   .recipe-card{
-    @media screen and(min-width:768px) and (orientation:landscape){
-      width:33%;
-      min-width:250px;
-      max-width:320px;
-    }
     width:100%;
     border: 2px solid black;
     min-height:250px;
     margin-top:20px;
+    background: black;
+
+    @media screen and(max-width:450px){
+       width:100%;
+    }
+    @media screen and(min-width:451px){
+      width:33%;
+      min-width:250px;
+      max-width:320px;
+    }
+
+    &__title{
+      text-shadow: 0 0 10px #fff, 0 0 20px #fff, 0 0 30px #e60073, 0 0 40px #e60073;
+      color:white;
+    }
+
+    .informations {
+      &__illustration {
+        display: block;
+        width:150px;
+        height:auto;
+
+        @media screen and(min-width:768px){
+          width:100%;
+          height:200px;
+        }
+      }
+    }
   }
 
 </style>
